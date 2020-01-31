c1b77548779f7d55d426bd59bee13fc9fa81adab
300ms <ilhansnmz44@gmail.com>
2020-01-31 20:19:04 +0300

    Lint errors are fixed

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Friendship, type: :model do
   before :each do
     @user = User.create(email: 'foobar1111@example.com', first_name: 'foobar', password: 'foobar')
     @friend = User.create(email: 'foobar2222@example.com', first_name: 'foobar2', password: 'foobar2')
-    @friendship = Friendship.create(:user => @user, :friend => @friend, :status => "pending")
+    @friendship = Friendship.create(user: @user, friend: @friend, status: 'pending')
   end
   context 'with valid user and friend details' do
     it 'should create a friendship(pending)' do
