cb15076273aa9a7fc377bcca305bc07da70dfb60
300ms <ilhansnmz44@gmail.com>
2020-01-31 19:23:33 +0300

    A test is added for friendship

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,4 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
+  @user = User.find(1)
+  @friend = User.find(2)
+  @friendship = Friendship.create(:user => @user, :friend => @friend, :status => "pending")
+
+  context 'with valid user and friend details' do
+    it 'should create a friendship(pending)' do
+      expect(@friendship).to be_valid
+    end
+  end
 end
