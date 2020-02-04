d01644c9d2edf2eaeca9d7a2f88a1973d1a7fb60
300ms <ilhansnmz44@gmail.com>
2020-02-04 10:21:00 +0300

    Linter errors after implementing tests have been corrected

--- spec/requests/friendships_spec.rb
+++ spec/requests/friendships_spec.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe "UsersLogins", type: :request do
-  describe "GET /users_logins" do
+RSpec.describe 'UsersLogins', type: :request do
+  describe 'GET /users_logins' do
     before :each do
       @user = User.create(email: 'user@example.com', password: 'password')
       @friend = User.create(email: 'user2@example.com', password: 'password')
       @user3 = User.create(id: 3, email: 'user3@example.com', password: 'password')
-      @friendship = Friendship.create(:user => @user, :friend => @friend, :status => 'pending')
+      @friendship = Friendship.create(user: @user, friend: @friend, status: 'pending')
     end
 
-    it "Successful Friendship" do
+    it 'Successful Friendship' do
       expect(@friendship).to be_valid
     end
 
-    it "Successful Friendship" do
+    it 'Successful Friendship' do
       get new_user_session_path
-      post "/users/sign_in", :params => { :user => {email: 'user@example.com', password: 'password'} }
+      post '/users/sign_in', params: { user: { email: 'user@example.com', password: 'password' } }
       expect(response).to redirect_to(root_path)
       follow_redirect!
       get users_path
-      post "/friendships?friend=3"
+      post '/friendships?friend=3'
       expect(Friendship.find_by_user_id_and_friend_id(@user, '3')).to be_valid
     end
   end
--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe "UsersLogins", type: :request do
-  describe "GET /users_logins" do
+RSpec.describe 'UsersLogins', type: :request do
+  describe 'GET /users_logins' do
     before :each do
       @user = User.create(email: 'user@example.com', password: 'password')
     end
 
-    it "Successful Login" do
+    it 'Successful Login' do
       get new_user_session_path
-      post "/users/sign_in", :params => { :user => {email: 'user@example.com', password: 'password'} }
+      post '/users/sign_in', params: { user: { email: 'user@example.com', password: 'password' } }
       expect(response).to redirect_to(root_path)
     end
 
-    it "Failed Login" do
+    it 'Failed Login' do
       get new_user_session_path
-      post "/users/sign_in", :params => { :user => {email: 'mistake@example.com', password: 'password'} }
+      post '/users/sign_in', params: { user: { email: 'mistake@example.com', password: 'password' } }
       expect(response).to_not redirect_to(root_path)
     end
   end
